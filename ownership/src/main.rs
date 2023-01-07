7cb9db44fadfb2e872e0651359cb3a135c521bb5
Thiago Bittencourt <git@thimabi.com>
2023-01-07 18:58:54 -0300

    Added ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
+
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
+}
